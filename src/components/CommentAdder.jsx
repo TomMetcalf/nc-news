1eba3331c71e1768c0cf8a3982d7aad9f9d63c4a
Tom Metcalf <intermet@gmail.com>
2023-05-31 16:55:42 +0100

    add form validation for comments

--- src/components/CommentAdder.jsx
+++ src/components/CommentAdder.jsx
@@ -2,22 +2,25 @@ import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
 import { postComment } from '../utils';
+import { Link } from 'react-router-dom';
 
 export default function CommentAdder({ setComments }) {
   const [newComment, setNewComment] = useState('');
   const { user } = useContext(UserContext);
   const { article_id } = useParams();
+  const [loginAlert, setLoginAlert] = useState(false);
+  const [commentAlert, setCommentAlert] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (user.username === 'Logged Out') {
-      alert('Please log in as a user to post a comment.');
+      setLoginAlert(true);
       return;
     }
 
     if (newComment.length === 0) {
-      alert('Please add a comment before posting!');
+      setCommentAlert(true);
       return;
     }
 
@@ -32,6 +35,10 @@ export default function CommentAdder({ setComments }) {
     );
   };
 
+  const handleTextareaFocus = () => {
+    setCommentAlert(false);
+  };
+
   return (
     <form className="comment-form" onSubmit={handleSubmit}>
       <label className="add-comment-label" htmlFor="new-comment">
@@ -42,6 +49,7 @@ export default function CommentAdder({ setComments }) {
         id="new-comment"
         mutliline="true"
         onChange={(e) => setNewComment(e.target.value)}
+        onFocus={handleTextareaFocus}
       ></textarea>
       <div className="form-username">
         <label className="username-details" htmlFor="">
@@ -49,6 +57,19 @@ export default function CommentAdder({ setComments }) {
         </label>
         <span className="username-details">{user.username}</span>
       </div>
+      {loginAlert && (
+        <div className='user-warning'>
+          <p className="comment-warning">
+            Please log in as a user to post a comment.
+            <Link className="comment-user-link" to={'/users'}>
+              Click to select User
+            </Link>
+          </p>
+        </div>
+      )}
+      {commentAlert && (
+        <p className="comment-warning">Please add a comment before posting!</p>
+      )}
       <button className="post-comment-btn">Post comment</button>
     </form>
   );
